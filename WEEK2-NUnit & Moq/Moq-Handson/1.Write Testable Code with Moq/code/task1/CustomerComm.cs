5190566ae1efc6c5e6085f281cea69b15f2cb7b9
banshitam12 <mishrabanshita@gmail.com>
2025-06-29 20:17:06 +0530

    Add files via upload

---
+++ WEEK2-NUnit & Moq/Moq-Handson/1.Write Testable Code with Moq/code/task1/CustomerComm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            //Actual logic goes here
+            //define message and mail address
+            _mailSender.SendMail("cust123@abc.com", "Some Message");
+
+            return true;
+        }
+    }
+}
+
