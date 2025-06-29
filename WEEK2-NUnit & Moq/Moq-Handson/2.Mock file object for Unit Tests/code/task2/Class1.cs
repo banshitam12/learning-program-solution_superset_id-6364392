f6199ae4484a9149f56aaa4e2522cb21949df6f8
banshitam12 <mishrabanshita@gmail.com>
2025-06-29 20:44:55 +0530

    Add files via upload

---
+++ WEEK2-NUnit & Moq/Moq-Handson/2.Mock file object for Unit Tests/code/task2/Class1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Moq;
+using MagicFilesLib;
+using System.Collections.Generic;
+
+[TestFixture]
+public class DirectoryExplorerTests
+{
+    private readonly string _file1 = "file.txt";
+    private readonly string _file2 = "file2.txt";
+    private Mock<IDirectoryExplorer> _mockDirectoryExplorer;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _mockDirectoryExplorer = new Mock<IDirectoryExplorer>();
+    }
+
+    [Test]
+    public void GetFiles_ShouldReturnMockedFiles()
+    {
+        // Arrange
+        var expectedFiles = new List<string> { _file1, _file2 };
+        _mockDirectoryExplorer.Setup(x => x.GetFiles(It.IsAny<string>()))
+                              .Returns(expectedFiles);
+
+        // Act
+        var result = _mockDirectoryExplorer.Object.GetFiles("C:\\TestPath");
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(2, result.Count);
+        Assert.IsTrue(result.Contains(_file1));
+        Assert.IsTrue(result.Contains(_file2));
+    }
+}
+
