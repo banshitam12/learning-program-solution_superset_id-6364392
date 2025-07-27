0f1f47af16de80b8863242e7f793b12999031400
banshitam12 <mishrabanshita@gmail.com>
2025-07-27 12:08:19 +0530

    Add files via upload

---
+++ WEEK-6 React/1. ReactJS-HOL/code/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    
+        <h1>Welcome to first session of React</h1>
+      
+      
+      
+  );
+}
+
+export default App;
