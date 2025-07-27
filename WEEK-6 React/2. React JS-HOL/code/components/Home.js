af7e771faa4912e7194628178a526c8b39c64bbd
banshitam12 <mishrabanshita@gmail.com>
2025-07-27 12:16:57 +0530

    Add files via upload

---
+++ WEEK-6 React/2. React JS-HOL/code/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h2>Welcome to the Home page of Student Management Portal</h2>
+    </div>
+  );
+};
+
+export default Home;
