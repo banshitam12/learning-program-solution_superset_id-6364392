d8aeab5371e3553d48c6b89b3bfdff548442c415
banshitam12 <mishrabanshita@gmail.com>
2025-08-13 09:24:53 +0530

    Add files via upload

---
+++ WEEK-6 React/6. React-JS HOL/code/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h2>Welcome to Cognizant Academy Trainers Portal</h2>
+            <p>Manage and view trainer information efficiently.</p>
+        </div>
+    );
+};
+
+export default Home;
